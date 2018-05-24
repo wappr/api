3bd65b7131f99ba5d2796b1608602c9bbd65ce12
Levi Durfee <levi.durfee@gmail.com>
2018-05-24 13:47:32 -0400

    return users

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
     public function retrieve(Request $request)
 	{
-		return 'contacts';
+		return User::get(['id', 'name']);
 	}
 }
