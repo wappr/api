2ee7e8e6170bf30118a25527f1ab7890f9fe072c
Levi Durfee <levi.durfee@gmail.com>
2018-05-24 12:34:37 -0400

    fix token creation / expiration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,8 @@ class AuthController extends Controller
 				'sub' => $user->id,
 				'name' => $user->name,
 				'email' => $user->email,
-				'iat' => strtotime('today + 7 days')
+                'iat' => time()
+				'exp' => strtotime('today + 7 days')
 			], env('JWT_SECRET')),
             'status' => 'success'
 		];
