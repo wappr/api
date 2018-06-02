91ac9e660ddfa7527457215073a31c33c99f242e
Levi Durfee <levi.durfee@gmail.com>
2018-06-02 18:13:53 -0400

    add more methods to token helper

--- app/Helpers/Token.php
+++ app/Helpers/Token.php
@@ -7,7 +7,41 @@ use \Firebase\JWT\SignatureInvalidException;
 
 class Token
 {
+    /**
+     * Get the User ID from a JWT.
+     *
+     * @param  string $jwt
+     * @return string
+     */
     public static function getUserId($jwt)
+    {
+        $decoded = Token::decode($jwt);
+
+        return $decoded->sub;
+    }
+
+    /**
+     * Check if a JWT is valid using the env secret.
+     *
+     * @param  string  $jtw
+     * @return boolean
+     */
+    public static function isValid($jtw)
+    {
+        if(Token::decode($jwt) == false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Decode a JWT.
+     *
+     * @param  string $jwt
+     * @return JWT
+     */
+    public static function decode($jwt)
     {
         try {
             $decoded = JWT::decode($jwt, env('JWT_SECRET'), ['HS256']);
@@ -17,6 +51,7 @@ class Token
             return false;
         }
 
-        return $decoded->sub;
+        return $decoded;
     }
+
 }
